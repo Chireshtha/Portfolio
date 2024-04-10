a5b5fc479b1a89888ce1b1da05d38822c02957e7
Chireshtha V <chire14vramirthy@gmail.com>
2024-04-10 14:35:58 +0530

    second commit

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,7 +11,7 @@ const Resume = () => {
       </div>
       <div className='resume_content'>
         <h2>You can view my resume</h2>
-        <button className='btn btn-primary'><Link to='https://drive.google.com/file/d/1sOaFdqRUrG8wT7qq1cICxMHh-u7O3u0X/view?usp=sharing' target='blank'><span>Download</span></Link></button>
+        <button className='btn btn-primary'><Link to='https://drive.google.com/file/d/1sj8I1D7gMKAIWDZvNyf3SN1ONyo2k-XJ/view?usp=drivesdk' target='blank'><span>Download</span></Link></button>
       </div>
     </div>
   )
