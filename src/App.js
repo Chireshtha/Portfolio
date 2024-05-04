f46f2e0530ec2321cd21cdc0f5a2629eadbdc2b3
Chireshtha V <chire14vramirthy@gmail.com>
2024-05-04 18:58:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
         <Home />
-        <About/>
-        <Projects/>
-        <Resume/>
-        <Contact/>
+        <About />
+        <Projects />
+        <Resume />
+        <Contact />
+        <Footer />
     </BrowserRouter>
   );
 }
